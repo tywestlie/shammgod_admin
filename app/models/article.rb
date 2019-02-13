e6ba480c7943ea5b6359f2739ed7698f03642b67
Tyler Westlie <tyler.westlie@gmail.com>
2019-02-12 19:40:49 -0700

    adds model, migrations and unit tests for articles

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+  belongs_to :user
+
+  validates_presence_of :title, :body
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_021731) do
+ActiveRecord::Schema.define(version: 2019_02_13_023745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_02_13_021731) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
